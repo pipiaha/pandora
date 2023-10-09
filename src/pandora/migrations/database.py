756bef31de089b26a1351e8fafa34b770939ee6a
pipiaha <393818655@qq.com>
2023-10-09 15:39:00 +0800

    fix: db session pool

--- src/pandora/migrations/database.py
+++ src/pandora/migrations/database.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, NullPool
 from sqlalchemy.orm import sessionmaker
 
 from ..exts.config import DATABASE_URI
 
-engine = create_engine(DATABASE_URI, echo=False)
+engine = create_engine(DATABASE_URI, echo=False, poolclass=NullPool)
 
 Session = sessionmaker(bind=engine)
 
